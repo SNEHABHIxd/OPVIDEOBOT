80cd6554b9293c9ffbcf1e4c2a834383ebedf660
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-02 21:15:18 +0530

    Update music.py

--- program/music.py
+++ program/music.py
@@ -78,7 +78,7 @@ async def ytdl(link):
 
         return 0, stderr.decode()
 
-@Client.on_message(command(["play", f"play@SNEHABHI_VIDEOBOT"]) & other_filters)
+@Client.on_message(command(["play", f"play@SNEHABHI_BOT"]) & other_filters)
 
 async def play(_, m: Message):
 
